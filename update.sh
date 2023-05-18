b86570d612f0082305e1999182d55da679f3024b
NanderTGA <65074195+NanderTGA@users.noreply.github.com>
2023-05-18 18:50:26 +0200

    Update update.sh
    remove local changes

--- update.sh
+++ update.sh
@@ -2,6 +2,7 @@
 # This script is ran daily at 18:00 (6:00PM) in my timezone
 cd "$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 pm2 stop yabluzo --watch
+git reset --hard
 git pull
 npm i
 npm audit fix
