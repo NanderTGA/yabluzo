2e4b3aa63e7df5670189d5f3b07c94a5a2b48750
NanderTGA <65074195+NanderTGA@users.noreply.github.com>
2023-05-18 13:41:30 +0200

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+# This script is ran daily at 18:00 (6:00PM) in my timezone
+cd "$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+pm2 stop yabluzo --watch
+git pull
+npm i
+npm audit fix
+pm2 start yabluzo
