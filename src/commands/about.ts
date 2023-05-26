de5bebcd4069c827064740bc90b99b5565d7e678
NanderTGA <65074195+NanderTGA@users.noreply.github.com>
2023-05-26 21:35:04 +0200

    fix spelling

--- src/commands/about.ts
+++ src/commands/about.ts
@@ -2,12 +2,12 @@ import { DefaultFileExport } from "../types";
 import checkVersion, { gitHash, gitBranch } from "../utils/version.js";
 
 import { createRequire } from "node:module";
-const { version } = createRequire(import.meta.url)("../package.json") as { version: string };
+const { version } = createRequire(import.meta.url)("../../package.json") as { version: string };
 
 const commands: DefaultFileExport = {
     about: () => `I'm Yabluzo, a bot developed by [NanderTGA](https://nandertga.ddns.net).
 Do you have any suggestions? Feel free to submit them using \`y!suggest\`!`
-+ (process.env.DEV == "true" ? "\n**This bot is a dev instanceof Yabluzo, expect bugs and unfinished work!**" : ""),
++ (process.env.DEV == "true" ? "\n**This bot is a development instance of Yabluzo, expect bugs and unfinished work!**" : ""),
 
     version: async reply => {
         reply("Checking version, please wait... (This command is being developed right now, this is just a preview of it)");
