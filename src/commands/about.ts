cb649705968f6a4cbfb2fd68de96ab31303d6bb2
NanderTGA <65074195+NanderTGA@users.noreply.github.com>
2023-05-26 23:17:19 +0200

    finish version command

--- src/commands/about.ts
+++ src/commands/about.ts
@@ -16,7 +16,7 @@ Do you have any suggestions? Feel free to submit them using \`y!suggest\`!`
 + (process.env.DEV == "true" ? "\n**This bot is a development instance of Yabluzo, expect bugs and unfinished work!**" : ""),
 
     version: async reply => {
-        reply("Checking version, please wait... (This command is being developed right now, this is just a preview of it)");
+        reply("Checking version, please wait...");
 
         const versionStatus = await checkVersion()
             .catch(reason => {
@@ -24,7 +24,7 @@ Do you have any suggestions? Feel free to submit them using \`y!suggest\`!`
                 return "(information unavailable)";
             });
         
-        return `Yabluzo version ${version} (${gitHash} at ${gitBranch})${process.env.DEV == "true" ? " (development instance)" : ""}.
+        return `Yabluzo version ${version} ([${gitHash}](https://github.com/NanderTGA/yabluzo/commit/${gitHash}) at [${gitBranch}](https://github.com/NanderTGA/yabluzo/tree/${gitBranch}))${process.env.DEV == "true" ? " (development instance)" : ""}.
 This version is ${versionStatus} compared to the code on [the github repo](https://github.com/NanderTGA/yabluzo)`;
     },
 
