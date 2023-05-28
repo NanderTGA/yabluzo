5e0034ffa0c6b54215cb5938a4e570a936332282
NanderTGA <65074195+NanderTGA@users.noreply.github.com>
2023-05-28 11:36:43 +0200

    refactor to use child.execSync

--- src/utils/version.ts
+++ src/utils/version.ts
@@ -3,29 +3,10 @@ import { Octokit } from "octokit";
 
 const octokit = new Octokit();
 
-/**
- * Gets the output of a process.
- * @param command The process to execute.
- * @returns The output of the spawned process.
- */
-export async function getOutput(command: string): Promise<string> {
-    return new Promise( (resolve, reject) => {
-        const processArguments = command.split(" ");
-        const processName = processArguments[0];
-
-        const process = child.spawn(processName, processArguments.slice(1));
-
-        if (!process.stdout || !process.stderr) throw new Error("Process could not be spawned");
-
-        process.stdout.on("data", (data: string) => resolve(data.toString().replaceAll("\n", "")));
-        process.stderr.on("data", (data: string) => reject(data.toString().replaceAll("\n", "")));
-    });
-}
-
 // These values don't change while running, so we only run this once.
-export const gitHash = await getOutput("git rev-parse HEAD");
-export const gitBranch = await getOutput("git rev-parse --abbrev-ref HEAD");
-export const gitStatus = await getOutput("git status --porcelain");
+export const gitHash = child.execSync("git rev-parse HEAD").toString().trim();
+export const gitBranch = child.execSync("git rev-parse --abbrev-ref HEAD").toString().trim();
+export const gitStatus = child.execSync("git status --porcelain").toString().trim();
 
 export async function getCommit(ref: string) {
     return await octokit.request("GET /repos/{owner}/{repo}/commits/{ref}", {
